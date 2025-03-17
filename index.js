e6a8debbfab5e5a53e13b8b2eb80b51140c6efb8
AnahiVera <vera.anahi.93@gmail.com>
2025-03-17 17:52:38 -0300

    3.15-3.18

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 require('dotenv').config()
 const express = require('express')
-const app = express()
 var morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
+const app = express()
 
-
+// Middleware
 app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
 
 morgan.token('post-data', (req) => {
     if (req.method === 'POST') {
@@ -18,6 +16,21 @@ morgan.token('post-data', (req) => {
     }
     return '-'; // Return '-' for non-POST requests
 });
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'))
+
+// Error handler middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+//ROUTES
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -29,14 +42,78 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-
-
-app.use((error, request, response, next) => {
-    console.error(error.stack)
-    response.status(500).send('Something broke!')
+app.get('/api/info', (request, response) => {
+    Person.countDocuments({}).then(count => {
+        const date = new Date()
+        response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+    })
 })
 
-const PORT = process.env.PORT 
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id).then(person => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'name or number missing' })
+    }
+
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+// Unknown endpoint middleware
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+
+// Error handler middleware
+app.use(errorHandler)
+
+
+// Start server
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
